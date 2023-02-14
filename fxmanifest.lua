be49ab432d8e71f06e2d4e9c57e67c9cef6b8dbe
Stuxxy <86432730+StuxxyOfficial@users.noreply.github.com>
2023-02-14 04:40:19 +0000

    manifest update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games {'gta5'}
 
 author 'stuxxy'
-version '1.0.0'
+version '1.0.1'
 
 lua54 'yes'
 
@@ -17,4 +17,8 @@ client_scripts {
 
 server_scripts {
 	'server/main.lua'
+}
+
+escrow_ignore {
+	'**/*.lua'
 }
